b9a56a642e41ff7b102eaefc56e1b49b3e0ddc02
Matthew McClure <github@mmcc.io>
2023-10-20 17:59:01 -0700

    fresh invite link

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       </header>
       <main>
         <section className={styles.section}>
-          <h2><a href="https://join.slack.com/t/video-dev/shared_invite/zt-227hwjlz8-Zo9L~dOwgFFlKRikCh3Hqw">Join the Slack Community</a></h2>
+          <h2><a href="https://join.slack.com/t/video-dev/shared_invite/zt-25t1vzuhc-pLrHBjSziQma3Lh3cjQreA">Join the Slack Community</a></h2>
         </section>
       </main>
       <footer className={styles.footer}>
