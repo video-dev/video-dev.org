ce23a568cae133c81d84e7775e4cbbb2d8404d0d
Matt McClure <github@mmcc.io>
2025-10-26 18:22:33 +0000

    add the discord link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Video Dev</title>
-        <meta name="description" content="A community for video engineers with a focus on open source" />
+        <meta
+          name="description"
+          content="A community for video engineers with a focus on open source"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -20,15 +23,33 @@ export default function Home() {
       </header>
       <main>
         <section className={styles.section}>
-          <h2><a href="https://join.slack.com/t/video-dev/shared_invite/zt-3eqxsoy80-K9JtdDrL4Z5_sI9_wP58Cw">Join the Slack Community</a></h2>
+          <h2>
+            Join the experimental <a href="/discord">Discord</a>.
+          </h2>
+        </section>
+        <section className={styles.section} style={{ marginTop: "2rem" }}>
+          <h3>
+            <a href="https://join.slack.com/t/video-dev/shared_invite/zt-3eqxsoy80-K9JtdDrL4Z5_sI9_wP58Cw">
+              Join the original Slack Community
+            </a>
+          </h3>
         </section>
       </main>
       <footer className={styles.footer}>
         <section className={styles.links}>
-          <a href="https://github.com/video-dev"><Image src="/github-mark.svg" width={35} height={35} alt="Github" /></a>
-          <a href="https://demuxed.com"><Image src="/demuxed-mark.svg" width={35} height={35} alt="Demuxed" /></a>
+          <a href="https://github.com/video-dev">
+            <Image src="/github-mark.svg" width={35} height={35} alt="Github" />
+          </a>
+          <a href="https://demuxed.com">
+            <Image
+              src="/demuxed-mark.svg"
+              width={35}
+              height={35}
+              alt="Demuxed"
+            />
+          </a>
         </section>
       </footer>
     </>
-  )
+  );
 }
